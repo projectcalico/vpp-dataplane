82bef7f1384eadac3865c0fdbe1b8b37a8ad18ed
Aloys Augustin <aloys.augustin@polytechnique.org>
2021-05-28 15:36:21 +0200

    vpp: add gro optimization and regenerate api

--- vpplink/binapi/vpp_clone_current.sh
+++ vpplink/binapi/vpp_clone_current.sh
@@ -15,7 +15,8 @@ fi
 
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/86/29386/9 && git cherry-pick FETCH_HEAD # 29386: virtio: DRAFT: multi tx support | https://gerrit.fd.io/r/c/vpp/+/29386
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/21/31321/10 && git cherry-pick FETCH_HEAD # 31321: devices: add support for pseudo header checksum | https://gerrit.fd.io/r/c/vpp/+/31321
-git fetch "https://gerrit.fd.io/r/vpp" refs/changes/69/31869/11 && git cherry-pick FETCH_HEAD # 31869: gso: do not try gro on small packets | https://gerrit.fd.io/r/c/vpp/+/31869
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/69/31869/12 && git cherry-pick FETCH_HEAD # 31869: gso: do not try gro on small packets | https://gerrit.fd.io/r/c/vpp/+/31869
+git fetch "https://gerrit.fd.io/r/vpp" refs/changes/76/32476/2 && git cherry-pick FETCH_HEAD # 32476: gso: handle push flag in gro | https://gerrit.fd.io/r/c/vpp/+/32476
 
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/82/32482/1 && git cherry-pick FETCH_HEAD # 32482: virtio: compute cksums in output no offload | https://gerrit.fd.io/r/c/vpp/+/32482
 git fetch "https://gerrit.fd.io/r/vpp" refs/changes/83/32483/1 && git cherry-pick FETCH_HEAD # 32483: virtio: Still init unused txq | https://gerrit.fd.io/r/c/vpp/+/32483
