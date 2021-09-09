9f1b1321381804b9c1ec9b35a764cfdba180e0ab
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2021-09-09 15:19:21 +0200

    agent: refactor cni server
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

---
+++ calico-vpp-agent/cni/pod_interface/loopback.go
@@ -0,0 +1,75 @@
+// Copyright (C) 2021 Cisco Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pod_interface
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/projectcalico/vpp-dataplane/calico-vpp-agent/cni/storage"
+	"github.com/projectcalico/vpp-dataplane/calico-vpp-agent/config"
+	"github.com/projectcalico/vpp-dataplane/vpplink"
+	"github.com/sirupsen/logrus"
+)
+
+type LoopbackPodInterfaceDriver struct {
+	PodInterfaceDriverData
+}
+
+func NewLoopbackPodInterfaceDriver(vpp *vpplink.VppLink, log *logrus.Entry) *LoopbackPodInterfaceDriver {
+	i := &LoopbackPodInterfaceDriver{}
+	i.vpp = vpp
+	i.log = log
+	i.name = "loopback"
+	return i
+}
+
+func (i *LoopbackPodInterfaceDriver) CreateInterface(podSpec *storage.LocalPodSpec, stack *vpplink.CleanupStack) (err error) {
+	swIfIndex, err := i.vpp.CreateLoopback(&config.ContainerSideMacAddress)
+	if err != nil {
+		return errors.Wrapf(err, "Error creating loopback")
+	} else {
+		stack.Push(i.vpp.DeleteLoopback, swIfIndex)
+	}
+	podSpec.LoopbackSwIfIndex = swIfIndex
+
+	for _, ipFamily := range vpplink.IpFamilies {
+		err = i.vpp.SetInterfaceVRF(swIfIndex, podSpec.VrfId, ipFamily.IsIp6)
+		if err != nil {
+			return errors.Wrapf(err, "Error setting loopback %d in per pod vrf", swIfIndex)
+		}
+	}
+
+	err = i.DoPodIfNatConfiguration(podSpec, stack, podSpec.LoopbackSwIfIndex)
+	if err != nil {
+		return err
+	}
+
+	for _, containerIP := range podSpec.GetContainerIps() {
+		err = i.vpp.AddInterfaceAddress(swIfIndex, containerIP)
+		if err != nil {
+			return errors.Wrapf(err, "Error adding address %s to pod loopback interface", containerIP)
+		}
+	}
+
+	return nil
+}
+
+func (i *LoopbackPodInterfaceDriver) DeleteInterface(podSpec *storage.LocalPodSpec) {
+	err := i.vpp.DeleteLoopback(podSpec.LoopbackSwIfIndex)
+	if err != nil {
+		i.log.Errorf("Error deleting Loopback %s", err)
+	}
+}
