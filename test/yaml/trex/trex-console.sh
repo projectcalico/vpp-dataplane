e5854f22c3d61c7cded8734b19e05f8bee0089ee
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2021-09-09 17:05:30 +0200

    test: Add trex yaml (#228)
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

---
+++ test/yaml/trex/trex-console.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function generate_trex_py () {
+	IP_ADDR=$(ip -4 addr show eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
+	export SRC_ADDRESS=$IP_ADDR
+	export DST_ADDRESS=${DST_ADDRESS:-1.2.3.4}
+	export DST_PORT=${DST_PORT:-4444}
+	export SRC_PORT=${SRC_PORT:-4444}
+	export SRC_PORT2=${SRC_PORT2:-$SRC_PORT}
+	echo "Using $SRC_ADDRESS:[$SRC_PORT..$SRC_PORT2] -> $DST_ADDRESS:$DST_PORT"
+	cat /trex-scripts/trex_template.py | envsubst > /trex-scripts/trex.py
+	echo "$ trex-console"
+	echo "$ start -f /trex-scripts/trex.py -p 0 -m 10mbps"
+	echo "## To show stats (use q to quit)"
+	echo "$ tui"
+	echo "## To update to full speed"
+	echo "$ update -m 100%"
+	echo "## To stop traffic generation"
+	echo "$ stop -a"
+}
+
+generate_trex_py
+
+export PYTHONPATH=/usr/local/share/trex-interactive
+export TREX_EXT_LIBS=/usr/local/share/trex-external_libs
+
+python3 -m trex.console.trex_console $@
