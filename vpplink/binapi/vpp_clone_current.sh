ce8a3fb1ae0e06b00fd188786f4dffbc06176dec
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2022-02-03 16:24:09 +0100

    Add SCTP support in VPP
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- vpplink/binapi/vpp_clone_current.sh
+++ vpplink/binapi/vpp_clone_current.sh
@@ -106,5 +106,6 @@ git_cherry_pick refs/changes/88/31588/1 # 31588: cnat: [WIP] no k8s maglev from 
 git_cherry_pick refs/changes/83/28083/21 # 28083: acl: acl-plugin custom policies |  https://gerrit.fd.io/r/c/vpp/+/28083
 git_cherry_pick refs/changes/13/28513/28 # 25813: capo: Calico Policies plugin | https://gerrit.fd.io/r/c/vpp/+/28513
 git_cherry_pick refs/changes/31/37131/11 # 37131: urpf: add mode for specific fib index lookup | https://gerrit.fd.io/r/c/vpp/+/37131/
+git_cherry_pick refs/changes/59/37259/2 # 37259: cnat: Add sctp support | https://gerrit.fd.io/r/c/vpp/+/37259
 # --------------- Dedicated plugins ---------------
 
