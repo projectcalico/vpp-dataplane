cdaeddcbce1ccbaa1d3668cd933059f0f27e87f0
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2022-02-24 15:29:19 +0100

    vpp: af-pkt fix kick
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- vpplink/binapi/vpp_clone_current.sh
+++ vpplink/binapi/vpp_clone_current.sh
@@ -94,6 +94,7 @@ git_cherry_pick refs/changes/26/34726/1 # 34726: interface: add buffer stats api
 git_cherry_pick refs/changes/52/34852/2 # 34852: memif: fix rx/txqueue RC on connected | https://gerrit.fd.io/r/c/vpp/+/34852
 git_cherry_pick refs/changes/09/35209/1 # 35209: cnat: Fix conflicting rsession | https://gerrit.fd.io/r/c/vpp/+/35209
 git_cherry_pick refs/changes/39/35439/1 # 35439: ip: fix assert in ip4_ttl_inc | https://gerrit.fd.io/r/c/vpp/+/35439
+git_cherry_pick refs/changes/38/35438/1 # 35438: af_packet: fix tx stall by retrying failed sendto | https://gerrit.fd.io/r/c/vpp/+/35438
 
 # --------------- Dedicated plugins ---------------
 git_cherry_pick refs/changes/64/33264/7 # 33264: pbl: Port based balancer | https://gerrit.fd.io/r/c/vpp/+/33264
