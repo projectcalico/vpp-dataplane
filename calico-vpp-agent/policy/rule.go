projectcalico/vpp-dataplane

commit be1cf49839b2e0d9ba5f6e44bf25b8b60795269d

agent: better logging
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

calico-vpp-agent/policy/rule.go
@@ -24,6 +24,7 @@ import (
 	"github.com/projectcalico/vpp-dataplane/calico-vpp-agent/proto"
 	"github.com/projectcalico/vpp-dataplane/vpplink"
 	"github.com/projectcalico/vpp-dataplane/vpplink/types"
+	"github.com/sirupsen/logrus"
 )
 
 type Rule struct {
@@ -43,6 +44,26 @@ type Rule struct {
 	SrcNotIPSetNames []string
 
 	DstIPPortSetNames []string
+}
+
+func (r *Rule) String() string {
+	s := fmt.Sprintf("[vpp-id=%d rid=%s", r.VppID, r.RuleID)
+
+	s += types.StrListToString(" dipport==", r.DstIPPortIPSetNames)
+	s += types.StrListToString(" dipport!=", r.DstNotIPPortIPSetNames)
+	s += types.StrListToString(" sipport==", r.SrcIPPortIPSetNames)
+	s += types.StrListToString(" sipport!=", r.SrcNotIPPortIPSetNames)
+
+	s += types.StrListToString(" dipset==", r.DstIPSetNames)
+	s += types.StrListToString(" dipset!=", r.DstNotIPSetNames)
+	s += types.StrListToString(" sipset==", r.SrcIPSetNames)
+	s += types.StrListToString(" sipset!=", r.SrcNotIPSetNames)
+
+	s += types.StrListToString(" dipportset==", r.DstIPPortSetNames)
+
+	s += "]"
+
+	return s
 }
 
 func fromProtoRule(r *proto.Rule) (rule *Rule, err error) {
@@ -280,11 +301,12 @@ func (r *Rule) Create(vpp *vpplink.VppLink, state *PolicyState) (err error) {
 		return errors.Wrap(err, "error creating rule")
 	}
 	r.VppID = id
+    logrus.Infof("policy(add) VPP rule=%s id=%d", r.Rule, id)
 	return nil
 }
 
 func (r *Rule) Delete(vpp *vpplink.VppLink) (err error) {
-	err = vpp.RuleDelete(r.VppID)
+    logrus.Infof("policy(del) VPP rule id=%d", r.VppID)
 	if err != nil {
 		return err
 	}

calico-vpp-agent/cmd/debug-state/debug-state.go
@@ -16,14 +16,18 @@
 package main
 
 import (
+	"fmt"
 	"flag"
+
 	"github.com/projectcalico/vpp-dataplane/calico-vpp-agent/cni/storage"
+	"github.com/projectcalico/vpp-dataplane/calico-vpp-agent/config"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	var fname string
-	flag.StringVar(&fname, "f", "/var/run/vpp/calico_vpp_pod_state", "Pod state path")
+	cniServerStateFile := fmt.Sprintf("%s%d", config.CniServerStateFile, storage.CniServerStateFileVersion)
+	flag.StringVar(&fname, "f", cniServerStateFile, "Pod state path")
 	flag.Parse()
 
 	st, err := storage.LoadCniServerState(fname)

calico-vpp-agent/cni/cni_server.go
@@ -175,7 +175,7 @@ func (s *Server) Add(ctx context.Context, request *pb.AddRequest) (*pb.AddReply,
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	s.log.Infof("Adding Pod %s", podSpec.String())
+	s.log.Infof("pod(add) spec=%s", podSpec.String())
 
 	existingSpec, ok := s.podInterfaceMap[podSpec.Key()]
 	if ok {
@@ -197,7 +197,7 @@ func (s *Server) Add(ctx context.Context, request *pb.AddRequest) (*pb.AddReply,
 	if err != nil {
 		s.log.Errorf("CNI state persist errored %v", err)
 	}
-	s.log.Infof("Done Adding Pod %s", podSpec.String())
+	s.log.Infof("pod(add) Done spec=%s", podSpec.String())
 	// XXX: container MAC doesn't make sense anymore, we just pass back a constant one.
 	// How does calico / k8s use it?
 	return &pb.AddReply{
@@ -219,7 +219,7 @@ func (s *Server) FetchNDataThreads() {
 			s.log.Error("Couldn't fullfill request [crypto=%d total=%d]", config.IpsecNbAsyncCryptoThread, nVppWorkers)
 			nDataThreads = nVppWorkers
 		}
-		s.log.Info("Using [data=%d crypto=%d]", nDataThreads, nVppWorkers-nDataThreads)
+		s.log.Info("Using ipsec workers [data=%d crypto=%d]", nDataThreads, nVppWorkers-nDataThreads)
 
 	}
 	s.memifDriver.NDataThreads = nDataThreads
@@ -301,18 +301,18 @@ func (s *Server) Del(ctx context.Context, request *pb.DelRequest) (*pb.DelReply,
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	s.log.Infof("Deleting pod %s", partialPodSpec.Key())
+	s.log.Infof("pod(del) key=%s", partialPodSpec.Key())
 	initialSpec, ok := s.podInterfaceMap[partialPodSpec.Key()]
 	if !ok {
-		s.log.Warnf("Unknown pod to delete")
+		s.log.Warnf("Unknown pod to delete key=%s", partialPodSpec.Key())
 	} else {
-		s.log.Infof("Deleting pod %s", initialSpec.String())
+		s.log.Infof("pod(del) spec=%s", initialSpec.String())
 		s.DelVppInterface(&initialSpec)
-		s.log.Infof("Done Deleting pod %s", initialSpec.String())
+		s.log.Infof("pod(del) Done! spec=%s", initialSpec.String())
 	}
 
 	delete(s.podInterfaceMap, initialSpec.Key())
-	err := storage.PersistCniServerState(s.podInterfaceMap, config.CniServerStateFile + fmt.Sprint(storage.CniServerStateFileVersion))
+	err := storage.PersistCniServerState(s.podInterfaceMap, config.CniServerStateFile+fmt.Sprint(storage.CniServerStateFileVersion))
 	if err != nil {
 		s.log.Errorf("CNI state persist errored %v", err)
 	}

calico-vpp-agent/cni/network_vpp.go
@@ -67,7 +67,7 @@ func (s *Server) hasVRF(podSpec *storage.LocalPodSpec) bool {
 		}
 	}
 
-    if hasIp4 != hasIp6 {
+	if hasIp4 != hasIp6 {
 		s.log.Warnf("Partial VRF state hasv4=%t hasv6=%t key=%s", hasIp4, hasIp6, podSpec.Key())
 	}
 
@@ -93,32 +93,32 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 
 	stack := s.vpp.NewCleanupStack()
 
-	s.log.Infof("Checking available buffers")
+	s.log.Infof("pod(add) Checking available buffers")
 	buffersNeeded, err := s.checkAvailableBuffers()
 	if err != nil {
 		goto err
 	}
 
-	s.log.Infof("Creating Pod VRF")
+	s.log.Infof("pod(add) VRF")
 	err = s.CreatePodVRF(podSpec, stack)
 	if err != nil {
 		goto err
 	}
 
-	s.log.Infof("Creating Pod loopback")
+	s.log.Infof("pod(add) loopback")
 	err = s.loopbackDriver.CreateInterface(podSpec, stack)
 	if err != nil {
 		goto err
 	}
 
-	s.log.Infof("Creating Pod tuntap")
+	s.log.Infof("pod(add) tuntap")
 	err = s.tuntapDriver.CreateInterface(podSpec, stack, doHostSideConf)
 	if err != nil {
 		goto err
 	}
 
 	if podSpec.EnableMemif && config.MemifEnabled {
-		s.log.Infof("Creating Pod memif")
+		s.log.Infof("pod(add) memif")
 		err = s.memifDriver.CreateInterface(podSpec, stack)
 		if err != nil {
 			goto err
@@ -126,7 +126,7 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 	}
 
 	if podSpec.EnableVCL && config.VCLEnabled {
-		s.log.Infof("Creating Pod VCL socket")
+		s.log.Infof("pod(add) VCL socket")
 		err = s.vclDriver.CreateInterface(podSpec, stack)
 		if err != nil {
 			goto err
@@ -135,7 +135,7 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 
 	/* Routes */
 	if podSpec.EnableVCL {
-		s.log.Infof("Setting up Pod Punt routes")
+		s.log.Infof("pod(add) Punt routes")
 		err = s.SetupPuntRoutes(podSpec, stack, podSpec.TunTapSwIfIndex)
 		if err != nil {
 			goto err
@@ -147,7 +147,7 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 	} else {
 		swIfIndex, isL3 := podSpec.GetParamsForIfType(podSpec.DefaultIfType)
 		if swIfIndex != types.InvalidID {
-			s.log.Infof("Adding Pod default routes to %d l3?:%t", swIfIndex, isL3)
+			s.log.Infof("pod(add) Default routes to swIfIndex=%d isL3=%t", swIfIndex, isL3)
 			err = s.RoutePodInterface(podSpec, stack, swIfIndex, isL3)
 			if err != nil {
 				goto err
@@ -158,7 +158,7 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 
 		swIfIndex, isL3 = podSpec.GetParamsForIfType(podSpec.PortFilteredIfType)
 		if swIfIndex != types.InvalidID {
-			s.log.Infof("Adding Pod PBL routes to %d l3?:%t", swIfIndex, isL3)
+			s.log.Infof("pod(add) PBL routes to %d l3?:%t", swIfIndex, isL3)
 			err = s.RoutePblPortsPodInterface(podSpec, stack, swIfIndex, isL3)
 			if err != nil {
 				goto err
@@ -166,7 +166,7 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 		}
 	}
 
-	s.log.Infof("Announcing Pod Addresses")
+	s.log.Infof("pod(add) announcing pod Addresses")
 	for _, containerIP := range podSpec.GetContainerIps() {
 		common.SendEvent(common.CalicoVppEvent{
 			Type: common.LocalPodAddressAdded,
@@ -174,7 +174,7 @@ func (s *Server) AddVppInterface(podSpec *storage.LocalPodSpec, doHostSideConf b
 		})
 	}
 
-	s.log.Infof("Adding HostPorts")
+	s.log.Infof("pod(add) HostPorts")
 	err = s.AddHostPort(podSpec, stack)
 	if err != nil {
 		goto err
@@ -208,40 +208,40 @@ func (s *Server) DelVppInterface(podSpec *storage.LocalPodSpec) {
 	/* Routes */
 	if podSpec.EnableVCL {
 		if podSpec.TunTapSwIfIndex != vpplink.InvalidID {
-			s.log.Infof("Deleting routes to podVRF")
+			s.log.Infof("pod(del) routes to podVRF")
 			s.DeleteVRFRoutesToPod(podSpec)
-			s.log.Infof("Deleting Pod punt routes")
+			s.log.Infof("pod(del) punt routes")
 			s.RemovePuntRoutes(podSpec, podSpec.TunTapSwIfIndex)
 		}
 	} else {
 		swIfIndex, _ := podSpec.GetParamsForIfType(podSpec.PortFilteredIfType)
 		if swIfIndex != types.InvalidID {
-			s.log.Infof("Deleting Pod PBL routes to %d", swIfIndex)
+			s.log.Infof("pod(del) PBL routes to %d", swIfIndex)
 			s.UnroutePblPortsPodInterface(podSpec, swIfIndex)
 		}
 		swIfIndex, _ = podSpec.GetParamsForIfType(podSpec.DefaultIfType)
 		if swIfIndex != types.InvalidID {
-			s.log.Infof("Deleting Pod default routes to %d", swIfIndex)
+			s.log.Infof("pod(del) default routes to %d", swIfIndex)
 			s.UnroutePodInterface(podSpec, swIfIndex)
 		}
 	}
 
 	/* Interfaces */
 	if podSpec.EnableVCL && config.VCLEnabled {
-		s.log.Infof("Deleting Pod VCL")
+		s.log.Infof("pod(del) VCL")
 		s.vclDriver.DeleteInterface(podSpec)
 	}
 	if podSpec.EnableMemif && config.MemifEnabled {
-		s.log.Infof("Deleting Pod memif")
+		s.log.Infof("pod(del) memif")
 		s.memifDriver.DeleteInterface(podSpec)
 	}
-	s.log.Infof("Deleting Pod tuntap")
+	s.log.Infof("pod(del) tuntap")
 	s.tuntapDriver.DeleteInterface(podSpec)
 
-	s.log.Infof("Deleting Pod loopback")
+	s.log.Infof("pod(del) loopback")
 	s.loopbackDriver.DeleteInterface(podSpec)
 
-	s.log.Infof("Deleting Pod VRF")
+	s.log.Infof("pod(del) VRF")
 	s.DeletePodVRF(podSpec)
 
 	common.SendEvent(common.CalicoVppEvent{

calico-vpp-agent/cni/network_vpp_hostports.go
@@ -42,7 +42,7 @@ func (s *Server) AddHostPort(podSpec *storage.LocalPodSpec, stack *vpplink.Clean
 				Proto:    hostPort.Protocol,
 				LbType:   types.DefaultLB,
 			}
-			s.log.Infof("pod(add) hostport entry=%s", entry.String())
+			s.log.Infof("pod(add) hostport %s", entry.String())
 			id, err := s.vpp.CnatTranslateAdd(entry)
 			if err != nil {
 				return err
@@ -61,9 +61,9 @@ func (s *Server) DelHostPort(podSpec *storage.LocalPodSpec) {
 		for _, hostPort := range initialSpec.HostPorts {
 			err := s.vpp.CnatTranslateDel(hostPort.EntryID)
 			if err != nil {
-				s.log.Errorf("(del) Error deleting entry with ID %s: %v", hostPort.EntryID, err)
+				s.log.Errorf("(del) Error deleting entry with ID %d: %v", hostPort.EntryID, err)
 			}
-			s.log.Infof("pod(del) hostport entry=%s", hostPort.EntryID)
+			s.log.Infof("pod(del) hostport entry=%d", hostPort.EntryID)
 		}
 	} else {
 		s.log.Warnf("Initial spec not found")

calico-vpp-agent/cni/network_vpp_routes.go
@@ -31,7 +31,7 @@ func (s *Server) RoutePodInterface(podSpec *storage.LocalPodSpec, stack *vpplink
 				SwIfIndex: swIfIndex,
 			}},
 		}
-		s.log.Infof("Add route [podVRF ->MainIF] %s", route.String())
+		s.log.Infof("pod(add) route [podVRF ->MainIF] %s", route.String())
 		err := s.vpp.RouteAdd(&route)
 		if err != nil {
 			return errors.Wrapf(err, "Cannot adding route [podVRF ->MainIF] %s", route.String())
@@ -39,7 +39,7 @@ func (s *Server) RoutePodInterface(podSpec *storage.LocalPodSpec, stack *vpplink
 			stack.Push(s.vpp.RouteDel, &route)
 		}
 		if !isL3 {
-			s.log.Infof("Adding neighbor if[%d] %s", swIfIndex, containerIP.IP.String())
+			s.log.Infof("pod(add) neighbor if[%d] %s", swIfIndex, containerIP.IP.String())
 			err = s.vpp.AddNeighbor(&types.Neighbor{
 				SwIfIndex:    swIfIndex,
 				IP:           containerIP.IP,
@@ -61,7 +61,7 @@ func (s *Server) UnroutePodInterface(podSpec *storage.LocalPodSpec, swIfIndex ui
 				SwIfIndex: swIfIndex,
 			}},
 		}
-		s.log.Infof("Del route [podVRF ->MainIF] %s", route.String())
+		s.log.Infof("pod(del) route [podVRF ->MainIF] %s", route.String())
 		err := s.vpp.RouteDel(&route)
 		if err != nil {
 			s.log.Warnf("Error deleting route [podVRF ->MainIF] %s : %s", route.String(), err)
@@ -96,7 +96,7 @@ func (s *Server) RoutePblPortsPodInterface(podSpec *storage.LocalPodSpec, stack 
 		}
 
 		vrfId := podSpec.GetVrfId(false) // pbl only supports v4 ?
-		s.log.Infof("Adding PBL client for %s VRF %d", containerIP.IP, vrfId)
+		s.log.Infof("pod(add) PBL client for %s VRF %d", containerIP.IP, vrfId)
 		pblIndex, err := s.vpp.AddPblClient(&client)
 		if err != nil {
 			return errors.Wrapf(err, "error adding PBL client for %s VRF %d", containerIP.IP, vrfId)
@@ -106,7 +106,7 @@ func (s *Server) RoutePblPortsPodInterface(podSpec *storage.LocalPodSpec, stack 
 		podSpec.PblIndexes = append(podSpec.PblIndexes, pblIndex)
 
 		if !isL3 {
-			s.log.Infof("Adding neighbor if[%d] %s", swIfIndex, containerIP.IP.String())
+			s.log.Infof("pod(add) neighbor if[%d] %s", swIfIndex, containerIP.IP.String())
 			err = s.vpp.AddNeighbor(&types.Neighbor{
 				SwIfIndex:    swIfIndex,
 				IP:           containerIP.IP,
@@ -122,7 +122,7 @@ func (s *Server) RoutePblPortsPodInterface(podSpec *storage.LocalPodSpec, stack 
 
 func (s *Server) UnroutePblPortsPodInterface(podSpec *storage.LocalPodSpec, swIfIndex uint32) {
 	for _, pblIndex := range podSpec.PblIndexes {
-		s.log.Infof("Deleting PBL client[%d]", pblIndex)
+		s.log.Infof("pod(del) PBL client[%d]", pblIndex)
 		err := s.vpp.DelPblClient(pblIndex)
 		if err != nil {
 			s.log.Warnf("Error deleting pbl conf %s", err)
@@ -146,7 +146,7 @@ func (s *Server) CreatePodVRF(podSpec *storage.LocalPodSpec, stack *vpplink.Clea
 
 	for _, ipFamily := range vpplink.IpFamilies {
 		vrfId := podSpec.GetVrfId(ipFamily.IsIp6)
-		s.log.Infof("Adding VRF %d %s default route via VRF %d", vrfId, ipFamily.Str, common.PodVRFIndex)
+		s.log.Infof("pod(add) VRF %d %s default route via VRF %d", vrfId, ipFamily.Str, common.PodVRFIndex)
 		err = s.vpp.AddDefaultRouteViaTable(podSpec.GetVrfId(ipFamily.IsIp6), common.PodVRFIndex, ipFamily.IsIp6)
 		if err != nil {
 			return errors.Wrapf(err, "error adding VRF %d %s default route via VRF %d", vrfId, ipFamily.Str, common.PodVRFIndex)
@@ -161,7 +161,7 @@ func (s *Server) DeletePodVRF(podSpec *storage.LocalPodSpec) {
 	var err error
 	for _, ipFamily := range vpplink.IpFamilies {
 		vrfId := podSpec.GetVrfId(ipFamily.IsIp6)
-		s.log.Infof("Deleting VRF %d %s default route via VRF %d", vrfId, ipFamily.Str, common.PodVRFIndex)
+		s.log.Infof("pod(del) VRF %d %s default route via VRF %d", vrfId, ipFamily.Str, common.PodVRFIndex)
 		err = s.vpp.DelDefaultRouteViaTable(vrfId, common.PodVRFIndex, ipFamily.IsIp6)
 		if err != nil {
 			s.log.Errorf("Error  VRF %d %s default route via VRF %d : %s", vrfId, ipFamily.Str, common.PodVRFIndex, err)
@@ -171,7 +171,7 @@ func (s *Server) DeletePodVRF(podSpec *storage.LocalPodSpec) {
 	for _, ipFamily := range vpplink.IpFamilies {
 		vrfId := podSpec.GetVrfId(ipFamily.IsIp6)
 		vrfName := getInterfaceVrfName(podSpec, ipFamily.Str)
-		s.log.Infof("Deleting VRF %d %s", vrfId, ipFamily.Str)
+		s.log.Infof("pod(del) VRF %d %s", vrfId, ipFamily.Str)
 		err = s.vpp.DelVRF(vrfId, ipFamily.IsIp6, vrfName)
 		if err != nil {
 			s.log.Errorf("Error deleting VRF %d %s : %s", vrfId, ipFamily.Str, err)
@@ -190,7 +190,7 @@ func (s *Server) CreateVRFRoutesToPod(podSpec *storage.LocalPodSpec, stack *vppl
 				SwIfIndex: types.InvalidID,
 			}},
 		}
-		s.log.Infof("Adding route [mainVRF ->PodVRF] %s", route.String())
+		s.log.Infof("pod(add) route [mainVRF->PodVRF] %s", route.String())
 		err := s.vpp.RouteAdd(&route)
 		if err != nil {
 			return errors.Wrapf(err, "error adding route [mainVRF ->PodVRF] %s", route.String())
@@ -213,7 +213,7 @@ func (s *Server) DeleteVRFRoutesToPod(podSpec *storage.LocalPodSpec) {
 				SwIfIndex: types.InvalidID,
 			}},
 		}
-		s.log.Infof("Deleting route [mainVRF ->PodVRF] %s", route.String())
+		s.log.Infof("pod(del) route [mainVRF->PodVRF] %s", route.String())
 		err = s.vpp.RouteDel(&route)
 		if err != nil {
 			s.log.Errorf("error deleting vpp side routes route [mainVRF ->PodVRF] %s : %s", route.String(), err)
@@ -230,7 +230,7 @@ func (s *Server) SetupPuntRoutes(podSpec *storage.LocalPodSpec, stack *vpplink.C
 			Dst:   containerIP,
 			Paths: []types.RoutePath{{SwIfIndex: swIfIndex}},
 		}
-		s.log.Infof("Adding route [puntVRF ->PuntIF] %s", route.String())
+		s.log.Infof("pod(add) route [puntVRF->PuntIF] %s", route.String())
 		err = s.vpp.RouteAdd(&route)
 		if err != nil {
 			return errors.Wrapf(err, "error adding vpp side routes for interface")
@@ -250,7 +250,7 @@ func (s *Server) RemovePuntRoutes(podSpec *storage.LocalPodSpec, swIfIndex uint3
 			Dst:   containerIP,
 			Paths: []types.RoutePath{{SwIfIndex: swIfIndex}},
 		}
-		s.log.Infof("Deleting route [puntVRF ->PuntIF] %s", route.String())
+		s.log.Infof("pod(del) route [puntVRF->PuntIF] %s", route.String())
 		err = s.vpp.RouteDel(&route)
 		if err != nil {
 			s.log.Errorf("error deleting route [puntVRF ->PuntIF] %s : %s", route.String(), err)

calico-vpp-agent/cni/pod_interface/common.go
@@ -71,7 +71,7 @@ func (i *PodInterfaceDriverData) UndoPodIfNatConfiguration(swIfIndex uint32) {
 
 func (i *PodInterfaceDriverData) DoPodIfNatConfiguration(podSpec *storage.LocalPodSpec, stack *vpplink.CleanupStack, swIfIndex uint32) (err error) {
 	if podSpec.NeedsSnat {
-		i.log.Infof("Enable interface[%d] SNAT", swIfIndex)
+		i.log.Infof("pod(add) Enable interface[%d] SNAT", swIfIndex)
 		for _, ipFamily := range vpplink.IpFamilies {
 			err = i.vpp.EnableCnatSNAT(swIfIndex, ipFamily.IsIp6)
 			if err != nil {

calico-vpp-agent/cni/pod_interface/memif.go
@@ -112,6 +112,6 @@ func (i *MemifPodInterfaceDriver) DeleteInterface(podSpec *storage.LocalPodSpec)
 		}
 	}
 
-	i.log.Infof("Deleted memif[%d]", podSpec.MemifSwIfIndex)
+	i.log.Infof("pod(del) memif swIfIndex=%d", podSpec.MemifSwIfIndex)
 
 }

commit 8f0cea551f76db405760bcde2433b622eefe6a09

agent: rollback policy wrong commit
This was wronfully introduced by #327

Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

calico-vpp-agent/policy/rule.go
@@ -301,12 +301,13 @@ func (r *Rule) Create(vpp *vpplink.VppLink, state *PolicyState) (err error) {
 		return errors.Wrap(err, "error creating rule")
 	}
 	r.VppID = id
-    logrus.Infof("policy(add) VPP rule=%s id=%d", r.Rule, id)
+	logrus.Infof("policy(add) VPP rule=%s id=%d", r.Rule, id)
 	return nil
 }
 
 func (r *Rule) Delete(vpp *vpplink.VppLink) (err error) {
-    logrus.Infof("policy(del) VPP rule id=%d", r.VppID)
+	logrus.Infof("policy(del) VPP rule id=%d", r.VppID)
+	err = vpp.RuleDelete(r.VppID)
 	if err != nil {
 		return err
 	}