8462be14f1c29310eec7e0ecfa8eb95b7390481e
hedi bouattour <hedibouattour2010@gmail.com>
2022-05-18 16:42:00 +0000

    vpp: rebase and make goapi

--- vpplink/binapi/vpp_clone_current.sh
+++ vpplink/binapi/vpp_clone_current.sh
@@ -84,16 +84,12 @@ function git_clone_cd_and_reset ()
 
 # --------------- Things to cherry pick ---------------
 
-git_clone_cd_and_reset "$1" 45207e0fb2c46e211ff2e66fb141867d81198d97
+git_clone_cd_and_reset "$1" 38659d8f2c588500f4e844195e0c59ab1cf6107f
 
-git_cherry_pick refs/changes/12/33312/4 # 33312: sr: fix srv6 definition of behavior associated to a LocalSID | https://gerrit.fd.io/r/c/vpp/+/33312
 git_cherry_pick refs/changes/13/34713/3 # 34713: vppinfra: improve & test abstract socket | https://gerrit.fd.io/r/c/vpp/+/34713
 git_cherry_pick refs/changes/71/32271/15 # 32271: memif: add support for ns abstract sockets | https://gerrit.fd.io/r/c/vpp/+/32271
 git_cherry_pick refs/changes/34/34734/2 # 34734: memif: autogenerate socket_ids | https://gerrit.fd.io/r/c/vpp/+/34734
 git_cherry_pick refs/changes/26/34726/1 # 34726: interface: add buffer stats api | https://gerrit.fd.io/r/c/vpp/+/34726
-git_cherry_pick refs/changes/52/34852/2 # 34852: memif: fix rx/txqueue RC on connected | https://gerrit.fd.io/r/c/vpp/+/34852
-git_cherry_pick refs/changes/09/35209/1 # 35209: cnat: Fix conflicting rsession | https://gerrit.fd.io/r/c/vpp/+/35209
-git_cherry_pick refs/changes/39/35439/1 # 35439: ip: fix assert in ip4_ttl_inc | https://gerrit.fd.io/r/c/vpp/+/35439
 git_cherry_pick refs/changes/38/35438/1 # 35438: af_packet: fix tx stall by retrying failed sendto | https://gerrit.fd.io/r/c/vpp/+/35438
 git_cherry_pick refs/changes/05/35805/2 # 35805: dpdk: add intf tag to dev{} subinput | https://gerrit.fd.io/r/c/vpp/+/35805
 
