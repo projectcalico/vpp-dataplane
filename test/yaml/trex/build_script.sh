8752364d5dd0dfce4fd451af971e772c687fd2c1
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2025-01-14 18:17:14 +0100

    test: upgrade trex image to v3.06
    This patch upgrades the trex image we maintain for testing to trex v3.06
    
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- test/yaml/trex/build_script.sh
+++ test/yaml/trex/build_script.sh
@@ -6,11 +6,8 @@ mkdir -p ${BUILD_DIR}/trex_build
 cd ${BUILD_DIR}/trex_build
 
 if [ ! -d trex-core ]; then
-	git clone https://github.com/cisco-system-traffic-generator/trex-core.git
-	cd trex-core
-	git checkout ddce00f6fdea89850e0bf939fc82b96079fdcde5
-	git apply /scratch/patches/0000-memif-abstract-trex.patch
-	cd linux_dpdk
+	git clone -b v3.06 https://github.com/cisco-system-traffic-generator/trex-core.git
+	cd trex-core/linux_dpdk
 	./b configure
 fi
 
