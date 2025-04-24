63d34f202dc92290b8b0aeaccea4625a1ba76c5b
hedi bouattour <hedibouattour2010@gmail.com>
2025-04-24 15:50:57 +0000

    add vcl sidecar and document it
    include vcl sidecar build in CI pipeline
    
    CI: force rebuild to get libs

--- vpp-manager/images/ubuntu-build/build_script.sh
+++ vpp-manager/images/ubuntu-build/build_script.sh
@@ -2,6 +2,15 @@
 
 set -o errexit
 
+
+cd ${VPP_DIR}/extras/libmemif
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../libmemif-install
+make
+make install
+cp ../libmemif-install/lib/libmemif.so ${VPP_DIR}/extras/libmemif/build/lib/
+
 cd ${VPP_DIR}
 
 make build-release
