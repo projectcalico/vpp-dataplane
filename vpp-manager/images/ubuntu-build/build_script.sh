170e15be6f71edc675d46af78343665291b88d71
Adrian Villin <avillin@cisco.com>
2025-07-01 12:27:54 +0200

    allow running 'make vpp' from an arbitrary directory

--- vpp-manager/images/ubuntu-build/build_script.sh
+++ vpp-manager/images/ubuntu-build/build_script.sh
@@ -2,11 +2,10 @@
 
 set -o errexit
 
-cd ${VPP_MGR_DIR}/vpp_build
+cd ${VPP_DIR}
 
 make build-release
 if [ "${NO_BUILD_DEBS}" != "true" ]; then
 	rm -f ./build-root/*.deb ./build-root/*.changes ./build-root/*.buildinfo
 	make pkg-deb
 fi
-
