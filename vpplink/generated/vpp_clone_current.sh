355b76eb079f0242841633af7b03d558083e7086
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2025-07-18 12:11:19 +0200

    Make vpp_clone_current ignore merged change-ids
    This patch makes it so that vpp_clone_current ignores the
    patches that have a change-id that is already present in tree
    when asked for a cherry pick.
    
    This allows to specify any BASE commit, even one where patches
    have been upstreamed.
    
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- vpplink/generated/vpp_clone_current.sh
+++ vpplink/generated/vpp_clone_current.sh
@@ -30,7 +30,7 @@ function git_get_commit_from_refs ()
 	CACHE_F=$CACHE_DIR/$(echo $refs |sed s@refs/changes/@@g |sed s@/@_@g)
 	if $(commit_exists_f $CACHE_F); then
 		COMMIT_HASH=$(cat $CACHE_F)
-		green "Using cached $COMMIT_HASH"
+		blue "Using cached $COMMIT_HASH"
 	else
 		green "Fetching $refs"
 		git fetch "https://gerrit.fd.io/r/vpp" ${refs}
@@ -45,8 +45,18 @@ function git_cherry_pick ()
 	refs=$1
     blue "Cherry picking $refs..."
 	COMMIT_HASH=$(git_get_commit_from_refs $refs)
-	git cherry-pick $COMMIT_HASH || exit_and_print_help "Cherry pick" $refs
-	git commit --amend -m "gerrit:${refs#refs/changes/*/} $(git log -1 --pretty=%B)"
+	CHANGE_ID=$(git log -1 --format=%b ${COMMIT_HASH} | grep -E 'Change-Id:' | head -1 | cut -d' ' -f2)
+    blue "commmit-hash:$COMMIT_HASH"
+    blue "change-id:   $CHANGE_ID"
+
+	EXISTING_COMMIT=$(git --no-pager log --format=format:%H -1 --grep "Change-Id: $CHANGE_ID")
+	if [ -z "$EXISTING_COMMIT" ]; then
+		git cherry-pick $COMMIT_HASH || exit_and_print_help "Cherry pick" $refs
+		git commit --amend -m "gerrit:${refs#refs/changes/*/} $(git log -1 --pretty=%B)"
+		green "Did cherry pick ${refs} as $(git log -1 --pretty=%H)"
+	else
+		green "Not cherry pick ${refs} change-id in tree as ${EXISTING_COMMIT}"
+	fi
 }
 
 function git_apply_private ()
@@ -114,19 +124,18 @@ git_cherry_pick refs/changes/26/34726/3 # 34726: interface: add buffer stats api
 git_revert refs/changes/75/39675/5  # ip-neighbor: do not use sas to determine NS source address
 
 # Mohsin's set of patches addressing the gso/cksum offload issue
-if [ "$BASE" = "1573e751c5478d3914d26cdde153390967932d6b" ]; then
-	git_cherry_pick refs/changes/84/42184/6  # interface: add a new cap for virtual interfaces
-	git_cherry_pick refs/changes/85/42185/6  # vnet: add assert for offload flags in debug mode
-	git_cherry_pick refs/changes/86/42186/6  # tap: enable IPv4 checksum offload on interface
-	git_cherry_pick refs/changes/19/42419/5  # dpdk: fix the outer flags
-	git_cherry_pick refs/changes/81/43081/2  # interface: clear flags after checksum computation
-	git_cherry_pick refs/changes/91/42891/5  # ip: compute checksums before fragmentation if offloaded
-	git_cherry_pick refs/changes/82/43082/6  # ipip: fix the offload flags
-	git_cherry_pick refs/changes/84/43084/3  # af_packet: conditionally set checksum offload based on TCP/UDP offload flags
-	git_cherry_pick refs/changes/83/43083/3  # virtio: conditionally set checksum offload based on TCP/UDP offload flags
-	git_cherry_pick refs/changes/25/42425/8  # interface: add support for proper checksum handling
-	git_cherry_pick refs/changes/36/43336/3  # gso: fix ip fragment support for gso packet
-fi
+git_cherry_pick refs/changes/84/42184/6  # interface: add a new cap for virtual interfaces
+git_cherry_pick refs/changes/85/42185/6  # vnet: add assert for offload flags in debug mode
+git_cherry_pick refs/changes/86/42186/6  # tap: enable IPv4 checksum offload on interface
+git_cherry_pick refs/changes/19/42419/5  # dpdk: fix the outer flags
+git_cherry_pick refs/changes/81/43081/2  # interface: clear flags after checksum computation
+git_cherry_pick refs/changes/91/42891/5  # ip: compute checksums before fragmentation if offloaded
+git_cherry_pick refs/changes/82/43082/6  # ipip: fix the offload flags
+git_cherry_pick refs/changes/84/43084/3  # af_packet: conditionally set checksum offload based on TCP/UDP offload flags
+git_cherry_pick refs/changes/83/43083/3  # virtio: conditionally set checksum offload based on TCP/UDP offload flags
+git_cherry_pick refs/changes/25/42425/8  # interface: add support for proper checksum handling
+git_cherry_pick refs/changes/36/43336/3  # gso: fix ip fragment support for gso packet
+
 git_cherry_pick refs/changes/98/42598/12  # pg: add support for checksum offload
 git_cherry_pick refs/changes/76/42876/10  # gso: add support for ipip tso for phyiscal interfaces
 
