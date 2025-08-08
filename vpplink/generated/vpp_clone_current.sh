4f1720631d5dfbebe0bef6685e66b9d4453ae319
hedi bouattour <hedibouattour2010@gmail.com>
2025-08-08 10:08:05 +0000

    add policy default and profile default

--- vpplink/generated/vpp_clone_current.sh
+++ vpplink/generated/vpp_clone_current.sh
@@ -161,4 +161,4 @@ git_apply_private 0004-capo-Calico-Policies-plugin.patch
 git_apply_private 0006-ip-neighbor-preserve-interface-ll-receive-dpo.patch
 
 
-git_cherry_pick refs/changes/68/43468/5 https://gerrit.fd.io/r/c/vpp/+/43468
+git_cherry_pick refs/changes/68/43468/6 # capo: make default behavior configurable for policies and profiles
