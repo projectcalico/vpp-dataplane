2d5f2f0373c1330816101b3472aa46d167b53e27
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2025-09-19 16:07:24 +0200

    Refactor build to push debug and release images
    This patch refactors the build system and adds building
    debug and release images for vpp and vclsidecar.
    Important changes :
    - we remove the --squash option which was not used in CI anyway
    and that is deprecated in favor of staged builds.
    - we remove tarball compression as we do not gain much disk space
    but loose compression / decompression
    - we fix an issue in tarball creation that was missing .so.25.06
    style dynamic libraries
    - we always package gdb with the images (removing WITH_GDB)
    - Add Debug/Release builds for memif
    - Add `make kind DEBUG=true` option for debug images
    
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- vpp-manager/images/ubuntu-build/build_script.sh
+++ vpp-manager/images/ubuntu-build/build_script.sh
@@ -4,17 +4,47 @@ set -o errexit
 
 
 cd ${VPP_DIR}/extras/libmemif
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../libmemif-install
+rm -rf debug
+mkdir -p debug/build
+mkdir -p debug/install
+cd debug/build
+cmake ../.. \
+	-DCMAKE_INSTALL_PREFIX=../install \
+	-DCMAKE_BUILD_TYPE=Debug
 make
 make install
-cp ../libmemif-install/lib/libmemif.so ${VPP_DIR}/extras/libmemif/build/lib/
+
+cd ${VPP_DIR}/extras/libmemif
+rm -rf release
+mkdir -p release/build
+mkdir -p release/install
+cd release/build
+cmake ../.. \
+	-DCMAKE_INSTALL_PREFIX=../install \
+	-DCMAKE_BUILD_TYPE=Release
+make
+make install
 
 cd ${VPP_DIR}
 
+make build
 make build-release
-if [ "${NO_BUILD_DEBS}" != "true" ]; then
-	rm -f ./build-root/*.deb ./build-root/*.changes ./build-root/*.buildinfo
-	make pkg-deb
-fi
+
+rm -f ./build-root/*.deb \
+	./build-root/*.changes \
+	./build-root/*.buildinfo
+rm -rf ./build-root/debs
+
+mkdir -p ./build-root/debs/debug
+make pkg-deb-debug
+mv ./build-root/*.deb \
+	./build-root/*.changes \
+	./build-root/*.buildinfo \
+	./build-root/debs/debug
+
+mkdir -p ./build-root/debs/release
+make pkg-deb
+mv ./build-root/*.deb \
+	./build-root/*.changes \
+	./build-root/*.buildinfo \
+	./build-root/debs/release
