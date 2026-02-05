6a3ce75d82332d5ac4995a3141471fd2cb9daf6b
hedi bouattour <hedibouattour2010@gmail.com>
2026-02-05 16:32:07 +0000

    trex: allow multiple src addresses and fix mac address config

--- test/yaml/trex/trex-console.sh
+++ test/yaml/trex/trex-console.sh
@@ -2,12 +2,13 @@
 
 function generate_trex_py () {
 	IP_ADDR=$(ip -4 addr show eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
-	export SRC_ADDRESS=$IP_ADDR
+	export SRC_ADDRESS=${SRC_ADDRESS:-$IP_ADDR}
+	export SRC_ADDRESS2=${SRC_ADDRESS2:-$SRC_ADDRESS}
 	export DST_ADDRESS=${DST_ADDRESS:-1.2.3.4}
 	export DST_PORT=${DST_PORT:-4444}
 	export SRC_PORT=${SRC_PORT:-4444}
 	export SRC_PORT2=${SRC_PORT2:-$SRC_PORT}
-	echo "Using $SRC_ADDRESS:[$SRC_PORT..$SRC_PORT2] -> $DST_ADDRESS:$DST_PORT"
+	echo "Using [$SRC_ADDRESS..$SRC_ADDRESS2]:[$SRC_PORT..$SRC_PORT2] -> $DST_ADDRESS:$DST_PORT"
 	cat /trex-scripts/trex_template.py | envsubst > /trex-scripts/trex.py
 	echo "$ trex-console"
 	echo "$ start -f /trex-scripts/trex.py -p 0 -m 10mbps"
