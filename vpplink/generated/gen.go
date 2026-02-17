8c24f2c5724cd52307c427560d517720655234e5
hedi bouattour <hedibouattour2010@gmail.com>
2026-02-17 12:11:23 +0000

    move vpplink files that were previously generated by the vpplink repo

--- vpplink/generated/gen.go
+++ vpplink/generated/gen.go
@@ -3,9 +3,7 @@
 package generated
 
 import (
-	_ "github.com/calico-vpp/vpplink/pkg"
 	_ "go.fd.io/govpp/cmd/binapi-generator"
 )
 
-//go:generate go build -buildmode=plugin -o ./.bin/vpplink_plugin.so github.com/calico-vpp/vpplink/pkg
-//go:generate go run go.fd.io/govpp/cmd/binapi-generator --no-version-info --no-source-path-info --gen rpc,./.bin/vpplink_plugin.so -o ./bindings --input $VPP_DIR ikev2 gso arp interface ip ipip ipsec ip_neighbor tapv2 nat44_ed cnat af_packet feature ip6_nd punt vxlan af_xdp vlib virtio avf wireguard npol memif acl abf crypto_sw_scheduler sr rdma vmxnet3 pbl memclnt session vpe urpf classify ip_session_redirect
+//go:generate go run go.fd.io/govpp/cmd/binapi-generator --no-version-info --no-source-path-info --gen rpc -o ./bindings --input $VPP_DIR ikev2 gso arp interface ip ipip ipsec ip_neighbor tapv2 nat44_ed cnat af_packet feature ip6_nd punt vxlan af_xdp vlib virtio avf wireguard npol memif acl abf crypto_sw_scheduler sr rdma vmxnet3 pbl memclnt session vpe urpf classify ip_session_redirect
