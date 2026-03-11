b550e693d107f8ba13ee689d3e1fb056fae570ff
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2026-03-11 20:20:51 +0100

    vpp-manager: disable punting NAs toward the host (#940)
    This patch prevents neighbor advertisements from being
    punted toward the host. The expectation is that linux
    will issue NS that will hit NDproxy, thus we should
    consume NAs in VPP, and learn neighbors, but not transmit
    the packets to linux.
    
    This is especially useful in the case where we loose the
    node IP (e.g. when dhcpv6 lease renewal fails), as forwarding
    NAs destined to the nodeIP to linux with a default route
    towards VPP will result in VPP learning neighbors on the tap0
    instead of the uplink.
    
    We also include an evolution in ND proxy that removes the need
    to allowlist destination IPs that VPP will reply to. That way
    we can have the guarantee that all next hops in linux will
    be the MAC address of the Gateway, regardless of the routing
    on the node.
    
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- vpplink/generated/vpp_clone_current.sh
+++ vpplink/generated/vpp_clone_current.sh
@@ -123,7 +123,8 @@ git_cherry_pick refs/changes/69/43369/21 # 43369: cnat: converge new cnat implem
 git_cherry_pick refs/changes/50/44350/3 # 44350: vnet: fix unicast NA handling in ND proxy | https://gerrit.fd.io/r/c/vpp/+/44350
 git_cherry_pick refs/changes/03/44903/1 # 44903: vxlan: reset next_dpo on delete | https://gerrit.fd.io/r/c/vpp/+/44903
 git_cherry_pick refs/changes/66/44966/5 # 44966: ip-neighbor: fix missing solicited-node multicast MAC | https://gerrit.fd.io/r/c/vpp/+/44966
-
+git_cherry_pick refs/changes/99/45099/2 # 45099: ip6-nd: add nd-proxy all dst | https://gerrit.fd.io/r/c/vpp/+/45099
+git_cherry_pick refs/changes/46/45046/4 # 45046: ip6-nd: add punt reason for neigh advs | https://gerrit.fd.io/r/c/vpp/+/45046
 
 # --------------- private plugins ---------------
 # Generated with 'git format-patch --zero-commit -o ./patches/ HEAD^^^'
