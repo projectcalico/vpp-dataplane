2a49fa6722f887bcc52c2d9075e4383fdcf6a9ab
Adrian Villin <avillin@cisco.com>
2026-03-20 10:21:45 -0400

    cache "RUN apt..." layers in Dockerfiles
    This patches prevents us from invalidating the apt-get
    installed packages every time the commit message changes.
    
    We also introduce NO_CACHE=true to force builds without cache
    
    Finally we change GIT_COMMIT scheme so that it does not contain
    the commit message but just e.g. 'v3.29.4-rc14-4-g94a7dde44a'
    
    Signed-off-by: Adrian Villin <avillin@cisco.com>

--- common.mk
+++ common.mk
@@ -50,8 +50,11 @@ ifeq (${CODEBUILD_WEBHOOK_TRIGGER},branch/master)
 	ALSO_LATEST := y
 endif
 
-CLUSTER_NAME ?= kind-$(shell whoami)-$(shell git describe --always --abbrev=4)
+CLUSTER_NAME ?= kind-$(shell whoami)-$(shell git rev-parse --short=4 HEAD)
 DOCKER_BUILD_ARGS =  --network=host
 DOCKER_BUILD_ARGS += --build-arg http_proxy=${DOCKER_BUILD_PROXY}
 DOCKER_BUILD_ARGS += --build-arg https_proxy=${DOCKER_BUILD_PROXY}
-DOCKER_BUILD_ARGS += --build-arg GIT_COMMIT="$(shell git log -1 --oneline)"
+DOCKER_BUILD_ARGS += --build-arg GIT_COMMIT="$(shell git describe --always --abbrev=40 --dirty)"
+ifeq (${NO_CACHE},true)
+	DOCKER_BUILD_ARGS += --no-cache
+endif
